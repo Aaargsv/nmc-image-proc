7f7dc70ba43b3d60b3119f276a71b1a51cd9c36e
Alexey Alisin <akalisin@mai.education>
2022-07-11 17:21:40 +0300

    init commit

---
+++ main.c
@@ -0,0 +1,29 @@
+
+
+
+
+extern int nmppsPut_8s(int number, int skip) ;
+
+
+
+int main()
+{
+
+	int c;
+	
+	c =  nmppsPut_8s(100, 9);
+
+	
+	c =  nmppsPut_8s(100, 8);
+
+	
+	c =  nmppsPut_8s(100, 16);
+
+	
+	c =  nmppsPut_8s(100, 24);
+
+	
+	
+	return 0; 
+} 
+
