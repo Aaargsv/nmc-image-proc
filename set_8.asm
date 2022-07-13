ec90ff7e7a1ea04bca6291dd4c301301ce6855cc
Alexey Alisin <akalisin@mai.education>
2022-07-13 15:46:49 +0300

    set_8 is complete

---
+++ set_8.asm
@@ -0,0 +1,62 @@
+/*void set8(unsigned char *data_ptr, int index, unsigned char set_val)*/
+global _set_8: label;
+
+begin ".text"
+<_set_8>
+
+	ar5 = ar7 - 2;
+	
+	push ar0, gr0;
+	push ar1, gr1;
+	
+	ar0 = [--ar5]; //ar0 = data_ptr
+	gr7 = [--ar5]; //gr7 = index
+	gr0 = [--ar5] 		with gr1 = gr7 >> 2; //gr0 = set_val with gr1 = index / 4
+	
+	gr7 = [ar0+=gr1] 	with gr1 = gr7 << 30; //gr7 = required 32_byte_array
+	ar5 = ar0			with gr1 =>> 27;
+						with gr1++;
+						with gr1++;
+	
+	skip gr1;
+	
+	gr1 = 00FFFFFFh with gr0 <<= 24;
+	pop ar1, gr1 		with gr7 = gr1 and gr7;
+	pop ar0, gr0		with gr7 = gr7 or gr0;
+	delayed return;
+		[ar5] = gr7;
+	
+	
+	gr1 = FF00FFFFh with gr0 <<= 16;
+	pop ar1, gr1 		with gr7 = gr1 and gr7;
+	pop ar0, gr0		with gr7 = gr7 or gr0;
+	delayed return;
+		[ar5] = gr7;
+	
+	
+	gr1 = FFFF00FFh with gr0 <<= 8;
+	pop ar1, gr1 		with gr7 = gr1 and gr7;
+	pop ar0, gr0		with gr7 = gr7 or gr0;
+	delayed return;
+		[ar5] = gr7;
+	
+	
+	gr1 = FFFFFF00h;
+	pop ar1, gr1 		with gr7 = gr1 and gr7;
+	pop ar0, gr0		with gr7 = gr7 or gr0;
+	delayed return;
+		[ar5] = gr7;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+end ".text"
