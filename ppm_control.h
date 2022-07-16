0c31a547308f0da860253881f717c8f3ad083faa
Alexey Alisin <akalisin@mai.education>
2022-07-16 17:47:00 +0300

    add ppm header and c-file

---
+++ ppm_control.h
@@ -0,0 +1,17 @@
+#ifndef PPM_CONTROL_GUARD
+#define PPM_CONTROL_GUARD
+
+struct ppm_header {
+	unsigned char format[2],
+	int width,
+	int height,
+	int depth
+};
+
+typedef struct ppm_header ppm_header_t;
+
+extern int ppm_read_header(void *data_ptr, ppm_header_t *header_info);
+extern int ppm_save_header(ppm_header_t *header_info);
+					
+
+#endif
